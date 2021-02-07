7eb1352f03c2a03145777edf81e109e3ad341696
Niharika2404 <niharikang24@gmail.com>
2021-02-07 17:22:15 +0530

    Refactor - Switch case statement updated

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -3,22 +3,23 @@
 echo "Welcome to Employee Wage Computation Program."
 
 isPresent=1;
+isAbsent=0;
 isPartTime=2;
 wagePerHr=20;
 fullDay=8;
 dailyPayment=0;
 checkRandom=$((RANDOM%2))
-if [ $isPresent -eq $checkRandom ]
-then
-	echo "Employee is Present."
-	dailyPayment=$(($wagePerHr*$fullDay))
-	echo "Daily Payment is: " $dailyPayment
-elif [ $isPartTime -eq $checkRandom ];
-then
-	dailyPayment=$(($WagePerHr*$fullDay*$isPartTime))
 
-else
-	echo "Employee is Absent."
-	echo "Daily Payment is: " $dailyPayment
-fi
-
+case $checkRandom in
+			$isPresent)
+					dailyPayment=$(($wagePerHr*$fullDay))
+					echo "Daily Payment is: $dailyPayment"
+						;;
+				$isPartTime)
+					dailyPayment=$(($wagePerHr*$fullDay*$isPartTime))
+					echo "Daily PAyment is: $dailyPayment"
+						;;
+				$isAbsent)
+					echo "Daily Payment is: $dailyPayment"
+						;;
+esac
